264e53d2414554218f4d9a2bd1cb2dd32955a98c
YaakovHatam <6986221+YaakovHatam@users.noreply.github.com>
2024-05-29 10:15:40 +0300

    update

--- ls12/q2.js
+++ ls12/q2.js
@@ -16,15 +16,48 @@ function startGame() {
     document.getElementById('feedback').innerHTML = "התחיל משחק חדש, נא לנחש מספר בין 1 ל- 100";
     document.getElementById('geussInput').value = "";
     tries = 0;
+    alert(targetNumber)
 }
 
 function updateGameStatus(guess) {
+    /*
+    *   הפונקציה תציג את אחת ההודעות הבאות בפסקה עם id בשם feedback
+    *	כאשר הניחוש נמוך מדי: נמוך מדי, נסה שוב!  
+    *	כאשר הניחוש גבוה מדי: גבוה מדי, נסה שוב!  
+    *	כאשר הניחוש נכון:  נכון, המספר היה x. לקח לך y נסיוניות. הקש על כפתור "Restart Game״ כדי לשחק שוב.   כאשר X ו- Y מייצגים את המספר הסודי ומספר הניחושים בהתאמה.
+    בנוסף על הפונקציה לנהל את מספר הניסיונות של המשתמש 
+    *	להעלות את מספר הניסיונות של המשתמש באחד 
+    *	להציג הודעה בפסקה עם id בשם tries את מספר הניחושים שהשתמש וכמה שנשארו
+    *	אם נשאר רק ניחוש אחד הצג את ההודעה בצבע אדום
+    *	אם המשתמש מסיים את כל 5 הניחושים ללא הצלחה - הצג הודעה 
+        “You used all tries, you lose ב - alert והתחל את המשחק מהתחלה 
+
+    */
+
+    tries++;
+    document.getElementById('tries').innerHTML = `You used ${tries} tries so far, ${MAXTRIES - tries} tries left!`
+
+    document.getElementById('tries').className = MAXTRIES - tries == 1 ? 'red' : '';
+
+    let _html = '';
+    if (guess < targetNumber) {
+        _html = "נמוך מדי, נסה שוב";
+    } else if (guess > targetNumber) {
+        _html = "גבוה מדי, נסה שוב";
+    } else if (guess == targetNumber) {
+        _html = `נכון המספר היה ${targetNumber}. לקח לך ${tries} נסיונות. הקש על הכפתור Restart game כדי לשחק שוב`;
+    }
+    document.getElementById('feedback').innerHTML = _html;
+
+    if (tries == MAXTRIES) {
+        alert("You used all tries, you lose");
+        startGame();
+    }
 
 }
 
 function checkGuess() {
     /*
-    . (10 נקודות) כתוב פונקציית (checkGuess(
     *	הפונקציה מקבלת כפרמטר ניחוש ובודקת אם הניחוש תקין - מספר בין 1 ל - 100
     *	אם הניחוש אינו תקין הצג הודעת שגיאה למשתמש בעזרת alert.
     *	אם הניחוש תקין הפונקציה תקרא לפונקציה (updateGame(guess   עם הפרמטר guess
@@ -39,6 +72,16 @@ function checkGuess() {
     }
 }
 
-function provideHint() { }
+function provideHint() {
+    /*
+    *	כתוב קוד html שיוצר כפתור בשם hint (רמז). צור אירוע כך שעם הלחיצה על הכפתור תופעל הפונקציה  ()provideHint 
+    *	הפונקציה מציגה הודעה למשתמש שנותן רמז  - האם המספר שצריך לנחש הוא זוגי או אי זוגי . ההודעה תוצג באלמנט html עם ה- id בשם פידבק
+    */
+    document.getElementById('feedback').innerHTML = targetNumber % 2 == 0 ? "המספר זוגי" : "המספר אי זוגי";
+
+}
 
 startGame();
+
+// סעיף 5
+// document.querySelectorAll('p')
